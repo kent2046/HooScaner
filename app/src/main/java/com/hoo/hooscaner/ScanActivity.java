1d335705a652278ec246d1f5f3cad6431291d157
jiahua.huang <253837544@qq.com>
2019-04-25 19:09:51 +0800

    修改

--- app/src/main/java/com/hoo/hooscaner/ScanActivity.java
+++ app/src/main/java/com/hoo/hooscaner/ScanActivity.java
@@ -1,19 +1,25 @@
 package com.hoo.hooscaner;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import cn.bingoogolapple.qrcode.core.QRCodeView;
 import cn.bingoogolapple.qrcode.zbar.ZBarView;
 import cn.bingoogolapple.qrcode.zxing.ZXingView;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 import ryqr.Decoder;
 import ryqr.Ryqr;
 
-public class ScanActivity extends AppCompatActivity implements QRCodeView.Delegate, DataCallBackListener {
+public class ScanActivity extends AppCompatActivity implements QRCodeView.Delegate, DataCallBackListener, EasyPermissions.PermissionCallbacks {
 
     private ZXingView mZXingView;
     private TextView tvState, tvScan, tvOther;
@@ -24,6 +30,7 @@ public class ScanActivity extends AppCompatActivity implements QRCodeView.Delega
 
     private String mResult = "";
 
+    private static final int REQUEST_CODE_QRCODE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +57,14 @@ public class ScanActivity extends AppCompatActivity implements QRCodeView.Delega
     @Override
     protected void onStart() {
         super.onStart();
-        mZXingView.startCamera();
-        mZXingView.startSpotAndShowRect();
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, perms)) {
+            requestCodeQRCodePermissions();
+        } else {
+            mZXingView.startCamera();
+            mZXingView.startSpotAndShowRect();
+        }
+
     }
 
     @Override
@@ -185,6 +198,30 @@ public class ScanActivity extends AppCompatActivity implements QRCodeView.Delega
                 }
             }
         });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        finish();
+        startActivity(new Intent(ScanActivity.this, ScanActivity.class));
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        finish();
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE_QRCODE_PERMISSIONS)
+    private void requestCodeQRCodePermissions() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, perms)) {
+            EasyPermissions.requestPermissions(this, "扫描二维码需要打开相机和散光灯的权限", REQUEST_CODE_QRCODE_PERMISSIONS, perms);
+        }
     }
 }
